94c25850df7aab6d0ef04109e337cb75d4e07dc6
bhar143 <89736377+bhar143@users.noreply.github.com>
2021-09-22 10:48:18 +0530

    Add files via upload

---
+++ PriceComparsion.java
@@ -0,0 +1,15 @@
+package glassignment2;
+
+public class PriceComparsion {
+    int PriceComparsion ;
+
+	public void priceRose(boolean[] compared_share_price) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void priceDecline(boolean[] compared_share_price) {
+		// TODO Auto-generated method stub
+		
+	}
+}
